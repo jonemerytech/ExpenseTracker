2e24da59d8062eb4e96c0be7bfa48a7895cc85f7
Jonathan Emery <jonathane@carsons.local>
2020-02-25 10:02:35 -0500

    Updates

---
+++ precache-manifest.32c47ea892a04a42278f8dd338d17268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007ac0bc09c31d51f7e772c1b85cffc3",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "1249de01a2869cc83ac6",
+    "url": "/ExpenseTracker/static/css/main.73b46aa8.chunk.css"
+  },
+  {
+    "revision": "440b776fd947060527ec",
+    "url": "/ExpenseTracker/static/js/2.954b3d2b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ExpenseTracker/static/js/2.954b3d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1249de01a2869cc83ac6",
+    "url": "/ExpenseTracker/static/js/main.6b926723.chunk.js"
+  },
+  {
+    "revision": "30b054590ef5322bdeea",
+    "url": "/ExpenseTracker/static/js/runtime-main.3e910375.js"
+  }
+]);
